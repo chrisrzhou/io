e9e722427fe8641e806745fefbc4702e63406035
chrisrzhou <chrisrzhou@pm.me>
2019-12-20 15:41:29 -0800

    update index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 import PageLayout from 'layouts/PageLayout';
-import { getGithubSourceLink } from 'routes';
+import { getGithubLink, getGithubSourceLink } from 'routes';
 import { Flex } from 'ui';
 
 export default function IndexPage() {
   return (
     <PageLayout
       description="A digital imprint of myself on the web."
+      source={getGithubLink('io')}
       title=".io"
     >
       <Flex alignItems="center" flexGrow={1} justifyContent="center" mt="-10vh">
         <blockquote>
-          What <a href={getGithubSourceLink()}>data</a> I cannot render, I do
+          What <a href={getGithubSourceLink()}>data</a> I cannot render, <a href={getGithubLink()}>I</a> do
           not understand.
         </blockquote>
       </Flex>
